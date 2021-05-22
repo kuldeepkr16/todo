5b1b65f843a672f309fb7f3c9a20d22bcc4fc66e
kuldeepkr16 <kuldeepkr16@gmail.com>
2021-05-23 00:49:57 +0530

    before heroku

--- app.py
+++ app.py
@@ -35,11 +35,20 @@ def products():
     print(all_todo)
     return 'Hello, products page!'
 
-@app.route('/update')
-def update():
-    all_todo = Todo.query.all()
-    print(all_todo)
-    return 'Hello, products page!'
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+        
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
